d07a2594f06457fee13d1059860cb4d44eec27ed
mmm <proweb2008.m@gmail.com>
2024-08-31 22:55:36 +0500

    v 0.6

--- Components/RecentProjects.tsx
+++ Components/RecentProjects.tsx
@@ -19,9 +19,9 @@ const RecentProjects = () => {
               <PinContainer href={link} title={link}>
                 <div className='flex items-center justify-center h-[20vg] lg:h-[30vh] mb-10 sm:w-96 w-[80vw] relative overflow-hidden'>
                   <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d] '>
-                    <Image src='/bg.png' alt="bg-img" className='w-full h-full' width={100} height={100} />
+                    <Image priority={true} src='/bg.png' alt="bg-img" className='w-full h-full' width={100} height={100} />
                   </div>
-                  <Image width={1000} height={1000} src={img} alt={title} className='z-10 absolute bottom-0 h-full w-full' />
+                  <Image priority={true} width={1000} height={1000} src={img} alt={title} className='z-10 absolute bottom-0 h-full w-full' />
                 </div>
                 <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>{title}</h1>
                 <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>{des}</p>
@@ -30,7 +30,7 @@ const RecentProjects = () => {
                   <div className='flex items-center'>
                     {
                       iconLists.map((item, i) => (
-                        <div key={item} className=' border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center ' style={{ transform: `translateX(-${5 * i * 2}px)` }} >
+                        <div key={item} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center ' style={{ transform: `translateX(-${5 * i * 2}px)` }} >
                           <Image src={item} alt={item} className='p-2' width={38} height={38} />
                         </div>
                       ))
